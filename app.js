eebaaf9347e0721adc473076c8882420b48ba315
cinderfist <ericbilello@gmail.com>
2026-07-11 23:33:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
+const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTizAGyLQWLge0LKAxoXBVAmF2qy3nWJRRZyGY5Z7Vi5vWJ8WFAwNppX0bPl8ESXp84QFbfwZaMtxK0/pub?gid=0&single=true&output=csv";
+
+
+fetch(sheetURL)
+    .then(response => response.text())
+    .then(data => {
+
+        let rows = data.split("\n");
+
+        let output = "";
+
+        for (let i = 1; i < rows.length; i++) {
+
+            let columns = rows[i].split(",");
+
+            if (columns[0]) {
+
+                let spellName = columns[0];
+                let image = columns[3];
+
+                output += 
+                "<button class='spell-button' " +
+                "data-name=\"" + spellName + "\" " +
+                "data-image=\"" + image + "\">" +
+                spellName +
+                "</button><br>";
+
+            }
+
+        }
+
+        document.getElementById("spell-list").innerHTML = output;
+
+    });
+
+
+
+document.addEventListener("click", function(event) {
+
+    if (event.target.classList.contains("spell-button")) {
+
+        let spellName = event.target.getAttribute("data-name");
+        let image = event.target.getAttribute("data-image");
+
+
+        document.getElementById("spell-card").innerHTML =
+            "<h2>" + spellName + "</h2>" +
+            "<img src=\"" + image + "\">";
+
+    }
+
+});
 
